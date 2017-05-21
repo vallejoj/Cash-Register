027a04d9e5511d775d935ee39693ccd499fdef33
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-05-20 17:29:27 -1000

    Almost Done

--- js/calculator.js
+++ js/calculator.js
@@ -5,55 +5,36 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
-function calculatorModule() {
-  var total = 0;
+ /**
+  * Declare a function named `calculatorModule`
+  * this function will have two private variables declared inside of it.
+  * @variable PRIVATE { Number } `memory`
+  * @variable PRIVATE { Number } `total`
+  * @return {object} `calculator` object that can be used
+  */
+ function calculatorModule() {
 
-  function getTotal() {
-    return total;
-  }
+   function add(x,y) {
+     return x+y;
+   }
 
-  function add(x) {
-    total += x;
-    return total;
-  }
+   function subtract(x,y) {
+     return x-y;
+   }
 
-  function subtract(x) {
-    total -= x;
-    return total;
-  }
+   function multiply(x,y) {
+     return x*y;
+   }
 
-  function multiply(x) {
-    total *= x;
-    return total;
-  }
+   function divide(x,y) {
+     return x/y;
+   }
 
-  function divide(x) {
-    total /= x;
-    return total;
-  }
+   return {
+     add: add,
+     subtract: subtract,
+     multiply: multiply,
+     divide: divide
+   };
 
-  function clearMemory() {
-    memory = 0;
-    return memory;
-  }
-  function recallMemory() {
-    return memory;
-  }
-
-  function saveMemory() {
-    memory = total;
-    return memory;
-  }
-  return {
-    getTotal: getTotal,
-    add: add,
-    subtract: subtract,
-    multiply: multiply,
-    divide: divide,
-    recallMemory:recallMemory,
-    saveMemory:saveMemory,
-    clearMemory: clearMemory,
-  }
-
-}
-console.log(calculatorModule())
+ }
--- js/cash_register.js
+++ js/cash_register.js
@@ -5,9 +5,22 @@ var calculator = calculatorModule();
 //display area
 var display = document.querySelector('.display');
 
-//number buttons
+//numbers
+var total = 0;
+var memory = 0;
 
+var nums = [];
+
+var num1 = 0;
+var num2 = 0;
+
+var calculation;
 //caculation buttons
+var add = document.querySelector('#add');
+var subtract = document.querySelector('#subtract');
+var multiply = document.querySelector('#multiply');
+var divide = document.querySelector('#divide');
+var equal = document.querySelector('#equal');
 
 //main buttons
 var clear = document.querySelector('#clear');
@@ -15,31 +28,87 @@ var getBalance = document.querySelector('#get-balance');
 var depositCash = document.querySelector('#deposit-cash');
 var withdrawCash = document.querySelector('#withdraw-cash');
 
-var numbers=document.querySelectorAll('.number-container button');
-display.innerHTML = '$0.00';
+var numbers = document.querySelectorAll('.number-container button');
 
+// display.innerHTML = '$0.00';
 
-
-clear.addEventListener('click', function (event) {
-	display.innerHTML = '$0.00';
-});
-
+//functions
 for (var i = 0; i < numbers.length; i++) {
+	numbers[i].addEventListener('click', function(event){
+		// display.innerHTML=event.currentTarget.value;
 
-numbers[i].addEventListener('click', function(event){
-display.innerHTML=event.currentTarget.value
-});
+			nums.push(event.currentTarget.value);
+			display.innerHTML = parseFloat(nums.join(""));
+
+	});
+}
+function clearAll() {
+	display.innerHTML = '0';
+	nums = [];
 }
 
+add.addEventListener('click' ,function(event){
+  	num1 = parseFloat(nums.join(""));
+    calculation="add";
+    clearAll();
+
+})
+subtract.addEventListener('click' ,function(event){
+  	num1 = parseFloat(nums.join(""));
+    clearAll();
+calculation="subtract";
+})
+multiply.addEventListener('click' ,function(event){
+  	num1 = parseFloat(nums.join(""));
+    calculation="multiply";
+    clearAll();
+
+})
+divide.addEventListener('click' ,function(event){
+  	num1 = parseFloat(nums.join(""));
+    calculation="divide";
+    clearAll();
+})
+equal.addEventListener('click' ,function(event){
+  	num2 = parseFloat(nums.join(""));
+    console.log(num2);
+    console.log(calculation);
+    console.log(num1);
+    if (calculation==="add") {
+      memory=calculator.add(num1,num2);
+      display.innerHTML=memory;
+
+
+    }else if (calculation==="subtract") {
+      memory=calculator.subtract(num1,num2);
+      display.innerHTML=memory;
+
+  }else if (calculation==="multiply") {
+    memory=calculator.multiply(num1,num2);
+    display.innerHTML=memory;
+
+}else if (calculation==="divide") {
+  memory=calculator.divide(num1,num2);
+  display.innerHTML=memory;
+  console.log(memory)
+}
+
+})
+clear.addEventListener('click', clearAll);
 
 getBalance.addEventListener('click', function(event) {
-
+	display.innerHTML = total;
 });
 
 depositCash.addEventListener('click', function(event) {
 
+	num1 = parseFloat(nums.join(""));
+	total = calculator.add(total, memory);
+	clearAll();
 });
 
 withdrawCash.addEventListener('click', function(event) {
-
+	num1 = parseFloat(nums.join(""));
+	total = calculator.subtract(total, num1);
+	clearAll();
 });
