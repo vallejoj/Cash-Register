5287126e192e4445cdd5f62166fac25149eadb43
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-05-24 16:18:50 -1000

    Css details.

--- js/cash_register.js
+++ js/cash_register.js
@@ -33,10 +33,7 @@ var withdrawCash = document.querySelector('#withdraw-cash');
 //number buttons
 var numbers = document.querySelectorAll('.number-container button');
 
-<<<<<<< HEAD
 display.innerHTML='0'
-=======
->>>>>>> 85985311577b6ee71d3bf2da0a08bbe5e1ddae19
 //functions
 // For loops through number buttons to check which one was clicked
 for (var i = 0; i < numbers.length; i++) {
@@ -52,14 +49,9 @@ for (var i = 0; i < numbers.length; i++) {
 
 // Function that clears the display as well as the nums array
 function clearAll() {
-<<<<<<< HEAD
   	display.innerHTML = "0";
   	nums = [];
-    memory=0
-=======
-  	display.innerHTML = "";
-  	nums = [];
->>>>>>> 85985311577b6ee71d3bf2da0a08bbe5e1ddae19
+    memory=0;
 }
 
 // When clicked it will call the clearAll() function
@@ -76,10 +68,7 @@ add.addEventListener('click', function(event) {
   		num1 = memory;
   	}
   	clearAll();
-<<<<<<< HEAD
         display.innerHTML='+';
-=======
->>>>>>> 85985311577b6ee71d3bf2da0a08bbe5e1ddae19
 });
 
 // After the (-) button is clicked it changes the calculation variable
@@ -93,10 +82,7 @@ subtract.addEventListener('click', function(event) {
   		num1 = memory;
   	}
   	clearAll();
-<<<<<<< HEAD
       display.innerHTML='-';
-=======
->>>>>>> 85985311577b6ee71d3bf2da0a08bbe5e1ddae19
 });
 
 // After the (*) button is clicked it changes the calculation variable
@@ -110,32 +96,21 @@ multiply.addEventListener('click', function(event) {
   		num1 = memory;
   	}
   	clearAll();
-<<<<<<< HEAD
   display.innerHTML='x';
-=======
-
->>>>>>> 85985311577b6ee71d3bf2da0a08bbe5e1ddae19
 });
 
 // After the (/) button is clicked it changes the calculation variable
 // to signify that an division equation will be performed and changes
 // the value of num1
 divide.addEventListener('click', function(event) {
-<<<<<<< HEAD
 	calculation = "divide";
-=======
-	calculation = "add";
->>>>>>> 85985311577b6ee71d3bf2da0a08bbe5e1ddae19
   	if(memory === 0) {
   		num1 = parseFloat(display.innerHTML);
   	} else {
   		num1 = memory;
   	}
   	clearAll();
-<<<<<<< HEAD
   display.innerHTML='÷';
-=======
->>>>>>> 85985311577b6ee71d3bf2da0a08bbe5e1ddae19
 });
 
 // After the (=) button is clicked it updates num2 and checks what type
@@ -150,177 +125,6 @@ equal.addEventListener('click', function(event) {
   	} else if (calculation === "subtract") {
   	  memory = calculator.subtract(num1, num2);
   	  display.innerHTML = memory;
-      //console.log('cash register sanity check');
-      //calculatorModule
-      var calculator = calculatorModule();
-
-      //display area
-      var display = document.querySelector('.display');
-
-      //numbers
-      var account = 0; //account
-      var memory = 0; //memory
-
-      var nums = []; //number array
-
-      var num1 = 0;
-      var num2 = 0;
-
-      //calculation string
-      var calculation;
-
-      //caculation buttons
-      var add = document.querySelector('#add');
-      var subtract = document.querySelector('#subtract');
-      var multiply = document.querySelector('#multiply');
-      var divide = document.querySelector('#divide');
-      var equal = document.querySelector('#equal');
-
-      //main buttons
-      var clear = document.querySelector('#clear');
-      var getBalance = document.querySelector('#get-balance');
-      var depositCash = document.querySelector('#deposit-cash');
-      var withdrawCash = document.querySelector('#withdraw-cash');
-
-      //number buttons
-      var numbers = document.querySelectorAll('.number-container button');
-
-      display.innerHTML='0'
-      //functions
-      // For loops through number buttons to check which one was clicked
-      for (var i = 0; i < numbers.length; i++) {
-        numbers[i].addEventListener('click', function(event) {
-          if(nums.indexOf(".") === -1) { //if '.' is not in nums, add to nums
-      		nums.push(event.currentTarget.value);
-          } else if (event.currentTarget.value != ".") { //if next val is != '.' add
-          	nums.push(event.currentTarget.value);
-          }
-          display.innerHTML = parseFloat(nums.join("")); //should we parse here?
-        });
-      }
-
-      // Function that clears the display as well as the nums array
-      function clearAll() {
-        	display.innerHTML = "0";
-        	nums = [];
-          memory=0
-      }
-
-      // When clicked it will call the clearAll() function
-      clear.addEventListener('click', clearAll);
-
-      // After the (+) button is clicked it changes the calculation variable
-      // to signify that an addition equation will be performed and changes
-      // the value of num1
-      add.addEventListener('click', function(event) {
-      	calculation = "add";
-        	if(memory === 0) {
-        		num1 = parseFloat(display.innerHTML);
-        	} else {
-        		num1 = memory;
-        	}
-        	clearAll();
-              display.innerHTML='+';
-      });
-
-      // After the (-) button is clicked it changes the calculation variable
-      // to signify that an subtraction equation will be performed and changes
-      // the value of num1
-      subtract.addEventListener('click', function(event) {
-      	calculation = "subtract";
-        	if(memory === 0) {
-        		num1 = parseFloat(display.innerHTML);
-        	} else {
-        		num1 = memory;
-        	}
-        	clearAll();
-            display.innerHTML='-';
-      });
-
-      // After the (*) button is clicked it changes the calculation variable
-      // to signify that an multiplication equation will be performed and changes
-      // the value of num1
-      multiply.addEventListener('click', function(event) {
-      	calculation = "multiply";
-        	if(memory === 0) {
-        		num1 = parseFloat(display.innerHTML);
-        	} else {
-        		num1 = memory;
-        	}
-        	clearAll();
-        display.innerHTML='x';
-      });
-
-      // After the (/) button is clicked it changes the calculation variable
-      // to signify that an division equation will be performed and changes
-      // the value of num1
-      divide.addEventListener('click', function(event) {
-      	calculation = "divide";
-        	if(memory === 0) {
-        		num1 = parseFloat(display.innerHTML);
-        	} else {
-        		num1 = memory;
-        	}
-        	clearAll();
-        display.innerHTML='÷';
-      });
-
-      // After the (=) button is clicked it updates num2 and checks what type
-      // of equation will need to be performed then updates the value of memory
-      // and updates the display to show the result
-      equal.addEventListener('click', function(event) {
-        	num2 = parseFloat(display.innerHTML);
-        	if (calculation === "add") {
-        	  memory = calculator.add(num1, num2);
-        	  display.innerHTML = memory;
-
-        	} else if (calculation === "subtract") {
-        	  memory = calculator.subtract(num1, num2);
-        	  display.innerHTML = memory;
-
-        	} else if (calculation === "multiply") {
-        	  memory = calculator.multiply(num1, num2);
-        	  display.innerHTML = memory;
-
-        	} else if (calculation === "divide") {
-        	  memory = calculator.divide(num1, num2);
-        	  display.innerHTML = memory;
-        	}
-
-      });
-
-      // Gets the balance of the account
-      getBalance.addEventListener('click', function(event) {
-        	display.innerHTML = account;
-      });
-
-      // Deposits cash into the account
-      depositCash.addEventListener('click', function(event) {
-      	var addCash = 0;
-      	if(display.innerHTML !== "") {
-      		addCash = parseFloat(display.innerHTML);
-      		if(addCash >= 0) { //can't deposit a negative number
-      			account = calculator.add(account, addCash);
-      		}
-      	}
-        clearAll();
-      });
-
-      // Withdraws cash from the account
-      withdrawCash.addEventListener('click', function(event) {
-      	var subtractCash = 0;
-      	if(display.innerHTML !== "") {
-      		subtractCash = parseFloat(display.innerHTML);
-      		if(account === 0 && subtractCash > 0) { //account === 0
-      			//alert can't withdraw from account
-      		} else if(subtractCash > account) { //can't withdraw more money than acc has
-      			//alert can't withdraw more money than acc has
-      		} else { //withdraw cash from account
-      			account = calculator.subtract(account, subtractCash);
-      		}
-      	}
-        	clearAll();
-      });
 
   	} else if (calculation === "multiply") {
   	  memory = calculator.multiply(num1, num2);
